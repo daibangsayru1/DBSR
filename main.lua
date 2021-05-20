68c66d12cc787e282db1574e639ec0c6098b47b7
daibangsayru1 <77434024+daibangsayru1@users.noreply.github.com>
2021-05-20 11:43:02 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,18 @@
----222
-function main(login clone full, login clone novery, restore rrs, file list clone, file list rrs, chạy very, api key dongvan, link lấy otp, úp danh bạ, úp avt, úp ảnh bìa, úp info, bậc 2fa, số lượt kb theo gợi ý, số lượt kb theo uid, đồng ý kết bạn, số lượt xem thông báo, số lượt join nhóm gợi ý, số lượt join nhóm theo list, file list nhóm, like page, file list page, số lượt vuốt new, số lượt like new, số lượt bình luận, tên file chứa nội dung bl, sl chia sẽ lên tường, đăng status, status kèm ảnh, xuất clone sau khi auto, link sheet lưu clone full, reg novery sau khi chạy, link sheet lưu novery, lưu rrs sau khi chạy)
+---333
+function main(loginclonefull, loginclonenovery, restorerrs, filelistclone, filelistrrs, chạyvery, apikeydongvan, linklấyotp, úpdanhbạ, úpavt, úpảnhbìa, úpinfo, bậc2fa, sốlượtkbtheogợiý, sốlượtkbtheouid, đồngýkếtbạn, sốlượtxemthôngbáo, sốlượtjoinnhómgợiý, sốlượtjoinnhómtheolist, filelistnhóm, likepage, filelistpage, sốlượtvuốtnew, sốlượtlikenew, sốlượtbìnhluận, tênfilechứanộidungbl, slchiasẽlêntường, đăngstatus, statuskèmảnh, xuấtclonesaukhiauto, linksheetlưuclonefull, regnoverysaukhichạy, linksheetlưunovery, lưurrssaukhichạy)
 	contair = require("socket.https")
-	if (login clone novery == "1") then
-		clone, cl2 = login(file list clone, login clone novery);
+	if (loginclonenovery == "1") then
+		clone, cl2 = login(filelistclone, loginclonenovery);
 		output = clone
 		if (cl2 == 0) then
 			return
 		end
 	end
-	if (lưu rrs sau khi chạy == "1") then
+	if (lưurrssaukhichạy == "1") then
 		local name = tachchuoi(output)
 		luurrs(name[1], output)
 	else
-		if (restore rrs ~= "1") then
+		if (restorerrs ~= "1") then
 			resetdata()
 		end
 	end
