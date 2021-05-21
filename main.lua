23357d0cf14c12d744f96c05d2a83ce95f5c42b5
daibangsayru1 <77434024+daibangsayru1@users.noreply.github.com>
2021-05-21 18:46:02 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,179 @@
 function main(loginclonefull, loginclonenovery, restorerrs, filelistclone, filelistrrs, chayvery, apikeydongvan, linklayotp, updanhba, upavt, upanhbia, upinfo, bac2fa, soluotkbtheogoiy, soluotkbtheouid, dongyketban, soluotxemthongbao, soluotjoinnhomgoiy, soluotjoinnhomtheolist, filelistnhom, likepage, filelistpage, soluotvuotnew, soluotlikenew, soluotbinhluan, tenfilechuanoidungbl, slchiaselentuong, dangstatus, statuskemanh, xuatclonesaukhiauto, linksheetluuclonefull, regnoverysaukhichay, linksheetluunovery, luurrssaukhichay);
 	contair = require("socket.https")
-	if (loginclonenovery == "1") then
-		clone, cl2 = login(filelistclone, loginclonenovery);
-		output = clone
-		if (cl2 == 0) then
+	if (loginclonefull == "1" or loginclonenovery == "1") then
+		clone, cl2 = login(filelistclone, loginclonenovery);---login
+	else
+	-----------------------restore RRS
+		clone = laydong1(filelistrrs)
+		writetxt("RRS đã chạy.txt", line, "a", 1, 1)
+		local ten = tachchuoi(clone)
+		local check = checkuid(ten[1])
+		if (check == 1) then
+			local f = io.popen("ls var/mobile/Media/XoaInfo")
+			tab = {}
+			repeat
+				line = f:read("*line")
+				tab[#tab+1] = line
+			until(line == nil)
+			f:close()
+			repeat
+				toast("Đang xóa RRS")
+				io.popen("rm -rf var/mobile/Media/XoaInfo/"..ten[1])
+				local f = io.popen("ls var/mobile/Media/XoaInfo")
+				tab2 = {}
+				repeat
+					line = f:read("*line")
+					tab2[#tab2+1] = line
+				until(line == nil)
+				f:close()
+			until(#tab > #tab2)
 			return
 		end
+		restore(ten[1])
+		local temp = tachchuoi(clone)
+		id, matkhau, key2fa = temp[1], temp[2], temp[3]
+		usleep(4000000)
+		appRun("com.facebook.Facebook")
+		usleep(2000000)
+		x = waitcolor(399, 1260, 15201279, 515, 777, 31487, 287, 774, 31487, 355, 767, 31487, 362, 767, 31487, 354, 136, 1603570, 394, 136, 1603570, 496, 1261, 1603570, 594, 1267, 1603570, 10, 1)
+		tapimg("xnok.jpg", 1, 1)
+		if (x == 355 or x == 362 or x == 354 or x == 394) then
+			tapimg("ok.jpg", 1, 2000000)
+			tapimg("ok2.jpg", 1, 2000000)
+			test = loginlai(temp[1],temp[2], temp[3])
+			tap(66, 1280)
+			usleep(2000000)
+			tap(66, 1280)
+			usleep(2000000)
+		else
+			local test2 = tapimg("dangnhap2.jpg", 1, 1000000)
+			if (test2 == 1) then
+				test = loginlai(temp[1],temp[2], temp[3])
+				tap(66, 1280)
+				usleep(2000000)
+				tap(66, 1280)
+				usleep(2000000)
+			end
+		end
+		local testlogin = waitcolor(35, 88, 1603570, 139, 90, 1603570, 496, 1261, 1603570, 594, 1267, 1603570, 20, 1)
+		local testok = tapimg("xnok.jpg", 1, 2000000)
+		if (testok == 1) then
+			testlogin = waitcolor(35, 88, 1603570, 139, 90, 1603570, 496, 1261, 1603570, 594, 1267, 1603570, 20, 1)
+		end
+		if (testlogin ~= 35 and testlogin ~= 139) then
+			toast("Ko login dc")
+			local f = io.popen("ls var/mobile/Media/XoaInfo")
+			tab = {}
+			repeat
+				line = f:read("*line")
+				tab[#tab+1] = line
+			until(line == nil)
+			f:close()
+			repeat
+				toast("Đang xóa RRS")
+				io.popen("rm -rf var/mobile/Media/XoaInfo/"..ten[1])
+				local f = io.popen("ls var/mobile/Media/XoaInfo")
+				tab2 = {}
+				repeat
+					line = f:read("*line")
+					tab2[#tab2+1] = line
+				until(line == nil)
+				f:close()
+			until(#tab > #tab2)
+			return
+		else
+			toast("Login ok")
+			testvr = 1
+		end
 	end
+	----------------------
+	if (cl2 == 0) then return end
+	----------------------
+	if (chayvery == "1") then
+		mail, mkmail, total = goimail(apikeydongvan)
+		testvr, cookie = chiveri(apikeydongvan, apiotp)
+	end
+	----------------------
+	if (testvr == 1) then
+		if (upavt == "1") then
+			upavt = upavatar();
+			if (upavt == 0) then
+				return
+			end
+		end
+		if (bac2fa == "1") then
+			key2fa, goiy = bacfa();
+			if (key2fa == "KEY2FA") then
+				return
+			end
+			clone = clone.."|"..key2fa.."|"..cookie.."|"..mail.."|"..mkmail
+		else
+			clone = clone.."|"..cookie.."|"..mail.."|"..mkmail
+		end
+	else
+		if (restorerrs == "1") then
+			io.popen("mv var/mobile/Media/XoaInfo/"..id.."/ var/mobile/Media/RRSLOI")
+		else
+			resetdata()
+		end
+		return
+	end
+	-------------------------------
+	thamgiagr(sljoin)
+	local testkb = ketban(slkb, sluid, 1, updb, id, matkhau, key2fa)
+	if (apigg ~= "0" and testvr == 1) then
+		local url = string.sub(apigg, 1, string.find(apigg, "entry")-2);
+		local entry = string.sub(apigg, string.find(apigg, "entry")+6, string.len(apigg))
+		local data = "--form-string 'entry."..entry.."="..clone.."'"
+		curlPost(url,data);
+	end
+	doctb(sltb)
+	luotnew(slvuot, sllike, slbl, slcs)
+	if (slreg ~= 0) then
+		local id, matkhau, test = novery(domain, kieureg, slkb, 1);
+		if (id ~= 0) then
+			ghichu = id.."|"..matkhau.."|"..test
+			tapimg("ok.jpg", 1, 1000000)
+			---tap(666, 1280)
+			---usleep(1000000)
+			--tap(666, 1280)
+			---usleep(1000000)
+			---tap(666, 1280)
+			---appKill("com.facebook.Facebook")
+			---usleep(1000000)
+			---appRun("com.facebook.Facebook")
+			---waitcolor(35, 88, 1603570, 139, 90, 1603570, 30, 1)
+			---tap(66, 1280)
+			---usleep(1000000)
+			---mail, mkmail, total = goimail(apimail)
+			---test3, cookie = chiveri(apimail, apiotp)
+			---if (test3 == 1) then
+				---ghichu = id.."|"..matkhau.."|"..mail.."|"..mkmail.."|"..os.date("%d")..os.date("%m").."_device_2"
+				---local url = string.sub(apigg, 1, string.find(apigg, "entry")-2);
+				---local entry = string.sub(apigg, string.find(apigg, "entry")+6, string.len(apigg))
+				---local data = "--form-string 'entry."..entry.."="..ghichu.."'"
+				---curlPost(url,data);	
+			---else
+			---------------------
+				local url = string.sub(ggnvr, 1, string.find(ggnvr, "entry")-2);
+				local entry = string.sub(ggnvr, string.find(ggnvr, "entry")+6, string.len(ggnvr))
+				local data = "--form-string 'entry."..entry.."="..ghichu.."'"
+				curlPost(url,data);	
+			---end
+			---local id2, matkhau2, test2 = novery(domain, kieureg, slkb, 1);
+			---if (id2 ~= 0) then
+				--ghichu2 = id2.."|"..matkhau2
+				--local url = string.sub(ggnvr, 1, string.find(ggnvr, "entry")-2);
+				--local entry = string.sub(ggnvr, string.find(ggnvr, "entry")+6, string.len(ggnvr))
+				--local data = "--form-string 'entry."..entry.."="..ghichu2.."'"
+				--curlPost(url,data);	 
+			--end
+		end
+	end
+	--------------------------------
 	if (luurrssaukhichay == "1") then
-		local name = tachchuoi(output)
-		luurrs(name[1], output)
+		local name = tachchuoi(clone)
+		luurrs(name[1], clone)
 	else
 		if (restorerrs ~= "1") then
 			resetdata()
