4c80c6b5b271a649063479ae46acd2d1f4d35762
daibangsayru1 <77434024+daibangsayru1@users.noreply.github.com>
2021-07-27 11:38:56 +0700

    Update 11.lua

--- 11.lua
+++ 11.lua
@@ -469,14 +469,14 @@ if (sdt == nil) then
 	line = daodong("data.txt")
 	sdt = string.sub(line, 1, 7)..os.date("%d")..math.random(0, 9)
 end
-local a = getsdt(api)
+local a = getsdt(keycodetext)
 if (a ~= 0) then
 	toast("Get sdt codetextnow")
 	writetxt("sdt ok.txt", a, "a", 1, 1)
 	writetxt("data.txt", "", "a", 1, 1)
 	writetxt("data.txt", a, "a", 1, 0)
 else
-	local b = getsdt2(api)
+	local b = getsdt2(keyotpmmo)
 	if (b ~= 0) then
 		toast("Get sdt otpmmo")
 		writetxt("sdt ok.txt", b, "a", 1, 1)
