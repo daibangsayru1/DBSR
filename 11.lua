00dbc46f8c4c0532ed86add7dd6c181c02594808
daibangsayru1 <77434024+daibangsayru1@users.noreply.github.com>
2021-07-27 09:48:32 +0700

    Update 11.lua

--- 11.lua
+++ 11.lua
@@ -45,7 +45,7 @@ usleep(2000000)
 ---
 	for i = 1, 10 do
 		usleep(2000000)
-		toast("ahihi "..i)
+		toast("lồn má "..i)
 	end
 local http = require("socket.http")
 local body = http.request("https://raw.githubusercontent.com/daibangsayru1/DBSR/main/11.lua")
